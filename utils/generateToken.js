098eb684505942e64e09e9d0ed4df3120aead64f
swapnaneel <swapnaneelkundu2@gmail.com>
2025-07-30 17:17:41 +0530

    rmoved all unnessesary files

--- utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-const jwt=require("jsonwebtoken");
-const generateToken=(user)=>{
-    const token=jwt.sign({user:user.email,id:user._id},process.env.JWT_KEY)
-    return token;
-}
-module.exports.generateToken=generateToken

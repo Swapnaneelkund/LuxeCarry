776a8f67d0823be731b24dae4bf1b376efee92b3
swapnaneel <swapnaneelkundu2@gmail.com>
2025-08-01 20:10:20 +0530

    bind 0.0.0.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 import logger from "./utils/logger.js";
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT,'0.0.0.0',()=>{
     logger.info('server started on port'+ process.env.PORT);
 })

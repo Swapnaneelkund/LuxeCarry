63e817115b1e8d74747b116bdea886f6607f1fbc
swapnaneel <swapnaneelkundu2@gmail.com>
2025-08-01 19:43:30 +0530

    Fix: use correct PORT env variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 import logger from "./utils/logger.js";
-app.listen(process.env.Port,()=>{
+app.listen(process.env.PORT,()=>{
     logger.info('server started on port'+ process.env.Port);
 })
